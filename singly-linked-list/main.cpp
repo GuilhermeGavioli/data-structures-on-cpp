5757f15ff4738ab98763092cf62e734239d43179
Guilherme Gavioli <xiiguilhermeiix@gmail.com>
2023-08-04 00:32:18 -0300

    add tail to the ll and quickadd algorithm takes O(1)

--- singly-linked-list/main.cpp
+++ singly-linked-list/main.cpp
@@ -31,24 +31,34 @@ public:
     Node* tail = NULL;
 
     LinkedList(){
-       
+       tail = new Node(0);
     }
 
     LinkedList(int* value){
         head = new Node(*value);
+        tail = new Node(0);
+        tail->next = head;
     }
 
     int add(Node* new_node){
         if (head == NULL){
             head = new_node;
+            tail->next = head;
             return 0;
         }
         return add_recursive(head, new_node);
     }
 
-    // add to the O(1) [instantly] by keeping track of the linked list tail
-    quick_add(){
-
+    // add to the O(1) time complexity by keeping track of the linked list tail (w/ a pointer on the tail of the ll)
+    bool quick_add(Node* node){
+        if (head == NULL){
+            head = node;
+            tail->next = head;
+            return 0;
+        }
+        tail->next->next = node;
+        tail->next = node;
+        return 0;
     }
 
     int remove(){
@@ -72,6 +82,9 @@ public:
     }
 
     int invert(){
+        if (head != NULL){
+            tail->next = head;
+        }
         return invert_recursive(NULL, head);
     }
 
@@ -87,13 +100,14 @@ private:
     int add_recursive(Node* curr, Node* new_node){
         if (curr->next == NULL){
             curr->next = new_node;
+            tail->next = new_node;
             return 0;
         }
         return add_recursive(curr->next, new_node);
     }
 
     void print_recursive(Node* node){
-        std::cout << node->value << "-->";
+        std::cout << node->value << " --> ";
         if (node->next == NULL){
             std::cout << " null";
             return;
@@ -104,6 +118,7 @@ private:
     int remove_recursive(Node* prev, Node* curr){
         if (curr->next == NULL){
             prev->next = NULL;
+            tail->next = prev;
             return 0;
         }
         prev = curr;
@@ -119,6 +134,7 @@ private:
     ){
         if(curr == NULL){
             prev->next = new_node;
+            tail->next = prev;
             return 0;
         }
         if (*curr_pos == *pos){ // Should insert here
@@ -146,26 +162,47 @@ private:
 
 int main() {
 
+    const int MAX = 6;
     LinkedList l = LinkedList();
-    for (int i = 1; i <= 10;  i++){
-     
+
+    for (int i = 1; i <= MAX;  i++){
             Node *node = new Node(i);
             l.add(node);
     }
 
+    for (int j = 1; j <= MAX;  j++){
+        Node *node = new Node(j + MAX);
+        l.quick_add(node);
+    }
+    l.print();
+
     Node *n1 = new Node(100);
     Node *n2 = new Node(200);
-    l.print();
+    Node *n3 = new Node(300);
+    Node *n4 = new Node(400);
     int pos = 5;
     int pos2 = 120;
-    l.addPosition(n1, &pos);
+    
+    // l.print();
+    // l.print();
+
+    
+    l.invert();
+    l.print();
+    l.quick_add(n3);
+    l.add(n4);
     l.print();
     l.addPosition(n2, &pos2);
+    l.addPosition(n1, &pos);
     l.print();
     l.invert();
     l.print();
 
 
     pause();
+    delete(n1);
+    delete(n2);
+    delete(n3);
     return 0;
+    exit(0);
 }
